af72ceeb712e7db8a9ba812b44d2bd0f301cd118
PlesneviyGRIB <90327075+PlesneviyGRIB@users.noreply.github.com>
2021-10-18 17:35:34 +0700

    j

---
+++ Stack/src/main/java/OBJ_stack.java
@@ -0,0 +1,79 @@
+/**
+ * @param <T> стэк
+ */
+public class OBJ_stack <T> {
+    private int cntElem = 10;
+    private T[] stack = (T[]) new Object[cntElem];
+
+    public OBJ_stack() {
+        cntElem = 0;
+        stack = (T[]) new Object[cntElem];
+    }
+
+    public int count() {
+        return cntElem;
+    }
+
+    /**
+     *
+     * @param elem элемент для пуша
+     */
+    public void push(T elem) {
+        T[] tmp = (T[]) new Object[cntElem];
+        for(int i=0;i< cntElem;i++){
+            tmp[i]= stack[i];
+        }
+        stack = (T[]) new Object[cntElem+1];
+        for(int i=0;i<cntElem;i++){
+            stack[i] = tmp[i];
+        }
+        stack[cntElem] = elem;
+        cntElem++;
+    }
+
+    /**
+     *
+     * @return элемент со стека
+     */
+    public T pop() {
+        if(cntElem>0) {
+            cntElem--;
+            return stack[cntElem];
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param stack1 вспомогательный стэк
+     */
+    public void pushStack(OBJ_stack<T> stack1){
+        OBJ_stack <T> stack2 = new OBJ_stack<>();
+        while(stack1.count()>0){
+            stack2.push(stack1.pop());
+        }
+        while(stack2.count()>0) {
+            push(stack2.pop());
+        }
+    }
+
+    /**
+     *
+     * @param cnt количество элементов дл взятия
+     * @return возвращает стек
+     */
+    public OBJ_stack popStack(int cnt){
+        OBJ_stack <T> stack1 = new OBJ_stack<>();
+        while(cnt > 0){
+            stack1.push(pop());
+            cnt--;
+        }
+
+        OBJ_stack <T> stack2 = new OBJ_stack<>();
+        while(stack1.count()>0){
+            stack2.push(stack1.pop());
+        }
+
+        return stack2;
+    }
+}
---
+++ Stack/src/test/java/T12.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class T12 {
+    @Test
+    public void Simple_test() {
+        OBJ_stack <Integer> stack = new OBJ_stack<>();
+        stack.push(10);
+        stack.push(11);
+        stack.push(90);
+        stack.push(12);
+        stack.push(17);
+
+        OBJ_stack <Integer> stack1 = new OBJ_stack<>();
+        stack1.push(1);
+        stack1.push(4);
+        stack1.push(9);
+        stack1.push(0);
+        stack.pushStack(stack1);
+
+        OBJ_stack <Integer> stack2 = stack.popStack(3);
+        OBJ_stack <Integer> stack3 = new OBJ_stack<>();
+        stack3.push(4);
+        stack3.push(9);
+        stack3.push(0);
+
+        for(int i=0;i<stack2.count();i++) {
+            Assertions.assertEquals(stack2.pop(),stack3.pop());
+        }
+    }
+}
