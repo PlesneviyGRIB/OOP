b808106aa8d787b690ee7bbced747bb1150bbb01
PlesneviyGRIB <unstoppablerina@gmail.com>
2022-04-02 13:31:27 +0700

    VS

---
+++ TestProject/TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class Program
+    {
+        private static readonly String path = "C:\\Users\\unsto\\source\\repos\\TestProject\\TestProject\\data.json";
+
+        public static void Main(string[] args)
+        {
+            double temperature = Convert.ToDouble(Console.ReadLine());
+
+            TimeSpan timeSpan = EngineStand.TestEngineOverheating(new InternalEngine(DataForSimulation.GetDataForSimulation(path), temperature));
+
+            if (timeSpan != TimeSpan.MaxValue) Console.WriteLine("{0:00}:{1:00}:{2:00}.{3:00}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds / 10);
+            else Console.WriteLine("engine will not overheat");
+        }
+    }
+}
