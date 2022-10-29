f2e1a9432129c79f8edc567568791efc1a8b89d0
PlesneviyGRIB <unstoppablerina@gmail.com>
2022-10-30 00:11:27 +0700

    1 changed

--- clojure/Task_1.clj
+++ clojure/Task_1.clj
@@ -1,8 +1,8 @@
-(defn removeListFromList [list1 list2]
-  (remove #(some #{%} list1) list2))
+(defn removeLastFromList [list1 list2]
+  (remove  #{apply list ( last list1)} list2))
 
 (defn foo [word, alphabet]
-  (apply list (mapv #(str word %)  (removeListFromList (apply list (clojure.string/split word #"")) alphabet))))
+  (apply list (mapv #(str word %)  (removeLastFromList (apply list (clojure.string/split word #"")) alphabet))))
 
 (defn bar [words, alphabet]
   (apply list (mapcat #(foo % alphabet) words)))
@@ -10,4 +10,4 @@
 (defn perm [alphabet n]
   (reduce bar (repeat n alphabet)))
 
-(print (perm (list "a" "b" "c" "d") 4))
+(print (perm (list "a" "b") 3))
