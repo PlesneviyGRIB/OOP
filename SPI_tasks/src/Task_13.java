a8231508d26f6d11a4c469f823e775962b38d297
PlesneviyGRIB <unstoppablerina@gmail.com>
2022-12-28 23:05:06 +0700

    13

--- SPI_tasks/src/Task_13.java
+++ SPI_tasks/src/Task_13.java
@@ -1,160 +1,88 @@
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
-// Sometimes it takes quite long to catch deadlock, u can change constants to do better.
-// U can determine deadlock condition if no new raws appearing in console.
-//
-// To reduce opportunity of deadlock one of philosopher should firstly pick up right fork (not all philosophers as first pick up fork from the same side).
 
 public class Task_13 {
     private static final int CNT = 5;
+    private static final int maxReflectionTime = 100;
+    private static final int maxMealTime = 100;
 
-    public static void main(String[] args) {
+    static class Philosopher extends Thread{
+        private final int id;
+        private final Semaphore leftFork;
+        private final Semaphore rightFork;
+        private final Semaphore waiter;
 
-        Runnable[] philosophers = new Runnable[CNT];
-        Fork[] forks = new Fork[CNT];
-
-        for(int i = 0; i<CNT; i++){
-            forks[i] = new Fork();
-        }
-
-        Waiter waiter = new Waiter();
-        philosophers[0] = new Philosopher(forks[CNT-1], forks[0], 0, waiter);
-        for(int i = 1; i<CNT; i++){
-            philosophers[i] = new Philosopher(forks[i-1], forks[i], i, waiter);
-        }
-
-        ExecutorService executorService = Executors.newCachedThreadPool();
-
-        List.of(philosophers).forEach(executorService::execute);
-
-        executorService.shutdown();
-    }
-    static class Philosopher implements Runnable{
-        private static final int maxReflectionTime = 4;
-        private static final int maxMealTime = 4;
-
-        private Fork leftFork;
-        private Fork rightFork;
-        private int id;
-        private Waiter waiter;
-
-        Philosopher(Fork leftFork, Fork rightFork, int id, Waiter waiter){
+        Philosopher(int id, Semaphore leftFork, Semaphore rightFork, Semaphore waiter) {
+            this.id = id;
             this.leftFork = leftFork;
             this.rightFork = rightFork;
-            this.id = id;
             this.waiter = waiter;
         }
 
         @Override
         public void run() {
-            while (true){
-                try {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
                     System.out.println("Philosopher " + id + " reflecting");
-                    TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, maxReflectionTime));
+
+
+                    TimeUnit.MILLISECONDS.sleep(maxReflectionTime);
+
+
+                    boolean state = false;
+
                     System.out.println("Philosopher " + id + " try eat");
-                    AtomicPickingUpBothForks atomic = new AtomicPickingUpBothForks(leftFork, rightFork, waiter);
-                    while(!atomic.atomicPickUp()) {} // if philosopher want to eat he will eat.
-                    TimeUnit.MILLISECONDS.sleep(100);
-                    try {
-                        System.out.println("Philosopher " + id + " picked up left fork");
-                        System.out.println("Philosopher " + id + " picked up right fork");
-                        System.out.println("Philosopher " + id + " HAVING MEAL");
-                        TimeUnit.SECONDS.sleep(ThreadLocalRandom.current().nextInt(1, maxMealTime));
+
+                    while (!state) {
+
+                        waiter.acquire();
+
+                        if (leftFork.availablePermits() > 0 && rightFork.availablePermits() > 0) {
+                            leftFork.acquire();
+                            rightFork.acquire();
+                            state = true;
+                            waiter.release();
+
+                        } else {
+                            synchronized (Philosopher.class) {
+                                waiter.release();
+                                Philosopher.class.wait();
+                            }
+                        }
                     }
-                    finally {
-                        atomic.atomicRelease();
-                        System.out.println("Philosopher " + id + " released right fork");
-                        System.out.println("Philosopher " + id + " released left fork");
+
+                    System.out.println("Philosopher " + id + " HAVING MEAL");
+
+                    TimeUnit.MILLISECONDS.sleep(maxMealTime);
+
+                    System.out.println("Philosopher " + id + " FINISHED EATING");
+
+                    leftFork.release();
+                    rightFork.release();
+
+                    synchronized (Philosopher.class) {
+                        waiter.release();
+                        Philosopher.class.notifyAll();
                     }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
                 }
-            }
+            }catch (InterruptedException ignore){}
         }
     }
 
-    static class Waiter{
-        private Lock lock = new ReentrantLock();
+    public static void main(String[] args){
 
-        public void lock(){
-            lock.lock();
-        }
+        Philosopher[] philosophers = new Philosopher[CNT];
 
-        public synchronized void sleep() throws InterruptedException {
-            release();
-            this.wait();
-            lock();
-        }
+        Semaphore[] forks = new Semaphore[CNT];
 
-        public synchronized void newFreeForks() {
-            this.notifyAll();
-        }
+        Semaphore waiter = new Semaphore(1);
 
-        public void release(){
-            lock.unlock();
-        }
-    }
+        for (int i = 0;i < forks.length; ++i)
+            forks[i] = new Semaphore(1);
 
-    static class AtomicPickingUpBothForks{
-        private Fork right;
-        private Fork left;
-        private Waiter waiter;
+        for (int i = 0;i < philosophers.length; ++i)
+            philosophers[i] = new Philosopher(i, forks[i], forks[(i + 1) % forks.length], waiter);
 
-        AtomicPickingUpBothForks(Fork left, Fork right, Waiter waiter){
-            this.left = left;
-            this.right = right;
-            this.waiter = waiter;
-        }
-
-        private boolean atomicPickUp(){
-            try{
-                waiter.lock();
-                boolean l = false, r = false;
-                try {
-                    l = left.use();
-                    r = right.use();
-                    if(l && r) return true;
-                }   catch (InterruptedException e){
-                    if(l) left.release();
-                    if(r) right.release();
-                }
-                if(l) left.release();
-                if(r) right.release();
-                waiter.sleep();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } finally {
-                waiter.release();
-            }
-            return false;
-        }
-        private void atomicRelease(){
-            try{
-                waiter.lock();
-                right.release();
-                left.release();
-            }
-            finally {
-                waiter.newFreeForks();
-                waiter.release();
-            }
-        }
-    }
-
-    static class Fork{
-        private final Lock lock = new ReentrantLock();
-
-        private boolean use() throws InterruptedException {
-            return lock.tryLock();
-        }
-        private void release(){
-            lock.unlock();
-        }
+        for (Philosopher p : philosophers) p.start();
     }
 }
